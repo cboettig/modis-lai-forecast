af14e4231d8023a66e12200ebafa3bad50c40358
jwsmith <wsjohn2@vt.edu>
2023-06-22 10:40:16 -0600

    updated spatial_forecast_example with first example

--- spatial_forecast_example.R
+++ spatial_forecast_example.R
@@ -18,6 +18,14 @@ august_complex <- fire |> filter(FIRE_NAME == "AUGUST COMPLEX")
 
 box <- august_complex |> st_transform(crs=4326) |> st_bbox()
 
-cuberast <- ingest_planetary_data(collection = "modis-15A2H-061", asset_name = "Lai_500m", start_date = "2002-01-01", end_date = "2023-07-01", box = c("xmin" = -123, "ymin" = 39, "xmax" = -122, "ymax" = 40))
+## Example 1: 
 
+cuberast_ex1 <- ingest_planetary_data(collection = "modis-15A2H-061", asset_name = "Lai_500m", start_date = "2002-01-01", end_date = "2023-07-01", box = c("xmin" = -123, "ymin" = 39, "xmax" = -122, "ymax" = 40))
 
+## set forecast date. note that date
+## should be input in format yyyy-mm-dd
+forecast_date <- '2023-06-22'
+
+## generate climatology forecast using spat_climatology function
+spat_climatology(cuberast = cuberast_ex1,
+                 date = forecast_date)
