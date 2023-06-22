3282d2ffd006b71cf8e8ba95ade97bcbefe92fac
Emma Mendelsohn <mendelsohn@ecohealthalliance.org>
2023-06-22 17:12:21 -0600

    add target function workflow, some notes

--- spatial_forecast_example.R
+++ spatial_forecast_example.R
@@ -3,6 +3,8 @@ for (f in list.files(here::here("R"), full.names = TRUE)) source (f)
 
 # Bounding box ------------------------------------------------------------
 # TODO add function and documentation for how this was selected
+# shapefile to be added to github
+# pull box, mask
 
 # Ingest data ------------------------------------------------------------
 raster_cube <- ingest_planetary_data(start_date = "2002-01-01", 
@@ -10,26 +12,24 @@ raster_cube <- ingest_planetary_data(start_date = "2002-01-01",
                                      box = c("xmin" = -123, "ymin" = 39, "xmax" = -122, "ymax" = 40))
 
 # Generate targets dir/files ------------------------------------------------------------
-# TODO function to create target file
-# take the cube
-# gdal split for a given day (argument)
-# incorporate mask
-# return targets directory
+target_forecast_dir <- create_target_file(cuberast = raster_cube, 
+                                          date = '2023-06-22',
+                                          dir = 'targets')
 
 # Forecast ----------------------------------------------------------------
 ensemble_forecast_dir <- spat_climatology(cuberast = raster_cube,
-                                      date = '2023-06-22') 
+                                          date = '2023-06-22',
+                                          dir = 'climatology') 
 
 # Score ----------------------------------------------------------------
 scored_forecast_dir <- scoring_spat_ensemble(fc_dir = ensemble_forecast_dir,
-                                        target_dir = 'targets', # chnage to output of create_target
-                                        scores_dir = 'scores')
+                                             target_dir = target_forecast_dir,
+                                             scores_dir = 'scores')
 
 # Upload to AWS -----------------------------------------------------------
 # TODO
-# prediction ensembles
-# target file
-# scored
 
 
-# TODO automation logic (setting dates)
+# Next steps --------------------------------------------------------------
+# TODO deliverable: this file, .qmd, flowchart 
+# TODO automation logic (setting dates for data download and forecast forward)
